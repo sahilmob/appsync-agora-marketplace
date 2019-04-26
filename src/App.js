c50c72157f8f38fc65ea2aa1cf6fa4d55e213403
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-04-26 16:49:33 +0300

    setup authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
-import React from "react";
 import "./App.css";
 
-class App extends React.Component {
-  state = {};
+import { AmplifyTheme, Authenticator } from "aws-amplify-react";
+import { Auth, Hub } from "aws-amplify";
 
-  render() {
-    return <div>App</div>;
-  }
+import React from "react";
+
+class App extends React.Component {
+	state = {
+		user: null
+	};
+
+	componentDidMount() {
+		this.getUserData();
+		Hub.listen("auth", this, "onHubCapsule");
+	}
+
+	getUserData = async () => {
+		const user = await Auth.currentAuthenticatedUser();
+		user
+			? this.setState({
+					user
+			  })
+			: this.setState({
+					user: null
+			  });
+	};
+
+	onHubCapsule = capsule => {
+		switch (capsule.payload.event) {
+			case "signIn":
+				console.log("signed in");
+				this.getUserData();
+				break;
+			case "signUp":
+				console.log("signed up");
+				break;
+			case "signOut":
+				console.log("signed out");
+				this.setState({
+					user: null
+				});
+				break;
+			default:
+				return;
+		}
+	};
+
+	render() {
+		const { user } = this.state;
+		return !user ? <Authenticator theme={theme} /> : <div>App</div>;
+	}
 }
 
+const theme = {
+	...AmplifyTheme,
+	navBar: {
+		...AmplifyTheme.navBar,
+		backgroundColor: "#ffc0cb"
+	},
+	button: {
+		...AmplifyTheme.button,
+		backgroundColor: "var(--amazonOrange)"
+	},
+	sectionBody: {
+		...AmplifyTheme.sectionBody,
+		padding: "5px"
+	},
+	sectionHeader: {
+		...AmplifyTheme.sectionHeader,
+		backgroundColor: "var(--squidInk)"
+	}
+};
+
+// export default withAuthenticator(App, true, [], null, theme);
 export default App;
